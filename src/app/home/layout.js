187a786ea025e9c247bfaa31e3020e50c5c1ee72
AbdullahKhan203 <hk7485966@gmail.com>
2025-11-15 22:37:06 +0500

    also added CEO image in home page

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -3,11 +3,13 @@ import ConsultationInvite from "@/components/ConsultationInvite";
 import ContactInfo from "@/app/contact/components/ContactInfo";
 import Founder from "../about/components/Founder";
 import Clients from "./components/Clients";
+import CEO from "../about/components/CEO";
 export default function HomeLayout({ children }) {
   return (
     <div className="about-page-wrapper">
       {children}
-      <Founder />
+      {/* <Founder /> */}
+      <CEO />
        <ConsultationInvite/>
         <ContactInfo/>
         <Clients />
